2e8cb2e71e7cd8d735699a45e43b1d469d369827
Phobial <harshalpatilcsn2020@gmail.com>
2024-06-24 12:40:48 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,55 @@
+import img1 from "./table-1.jpg"
+import img2 from "./table-2.png"
+import img3 from "./table-3.png"
+import img4 from "./table-4.jpg"
+import img5 from "./table-5.jpg"
+import img6 from "./table-6.jpg"
+import img7 from "./table-7.jpg"
+import img8 from "./table-8.jpg"
+const siteDetails={
+    title: "Power Squad💪",
+    description: "Power Squad is an indoor venue for exercise and sports.",
+    contactDetails:"Ashok Stambh , Nashik , Maharashtra , India",
+    showFeatures: true,
+}
+const features=[
+    {
+        title: "Variety of Training ",
+        img:img1,
+    },
+    {
+        title: "High-Quality Exercise Equipment",
+        img:img2,
+    },
+    {
+        title: "Well-Designed Space and Facility",
+        img:img3,
+    },
+    {
+        title: "Digital Fitness Platform",
+        img:img4,
+    },
+    {
+        title: "Well-Designed Space and Facility",
+        img:img5,
+    },
+    {
+        title: "Dyson Abstract Metal Coffee Table In Teak Finish",
+        img:img6,
+    },
+    {
+        title: "Well-Designed Space and Facility",
+        img:img7,
+    },
+    {
+        title: "Well-Designed Space and Facility",
+        img:img8,
+    }
+]
+const theme={
+    primaryColor:"#b3ffff",
+    secondaryColor:" #008080",
+    text:"#004d4d"
+}
+
+export {siteDetails,features,theme}
